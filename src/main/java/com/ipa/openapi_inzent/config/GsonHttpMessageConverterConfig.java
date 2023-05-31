c02f8676d6645bfd3cde98be08771199c9b5a32b
jinujin <espin97@naver.com>
2023-05-31 21:52:59 +0900

    05/31 21:30 swagger with gson

--- src/main/java/com/ipa/openapi_inzent/config/GsonHttpMessageConverterConfig.java
+++ src/main/java/com/ipa/openapi_inzent/config/GsonHttpMessageConverterConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.GsonHttpMessageConverter;
 import springfox.documentation.spring.web.json.Json;
-
+//
 @Configuration
 public class GsonHttpMessageConverterConfig {
 
