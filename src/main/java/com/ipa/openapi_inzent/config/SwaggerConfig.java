3ed05290fb66032517f41451cc6944a60d6fb09b
(본권주희 <ttyj77@naver.com>
2023-05-31 21:42:34 +0900

    swagger 설정 변경 중 - 주희

--- src/main/java/com/ipa/openapi_inzent/config/SwaggerConfig.java
+++ src/main/java/com/ipa/openapi_inzent/config/SwaggerConfig.java
@@ -1,19 +1,37 @@
 package com.ipa.openapi_inzent.config;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.GsonHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.json.Json;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 @Configuration
 @EnableWebMvc
-public class SwaggerConfig {
+public class SwaggerConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("classpath:static/");
+    }
+
+    private final Gson gson = new Gson();
 
     private ApiInfo swaggerInfo() {
         return new ApiInfoBuilder().title("OPEN API")
@@ -36,4 +54,49 @@ public class SwaggerConfig {
     public InternalResourceViewResolver defaultViewResolver() {
         return new InternalResourceViewResolver();
     }
+
+    @Bean
+    public GsonHttpMessageConverter gsonHttpMessageConverter(GsonBuilder gsonBuilder) {
+        GsonHttpMessageConverter converter = new GsonHttpMessageConverter();
+        gsonBuilder.registerTypeAdapter(Json.class, SpringfoxJsonToGsonAdapter.builder().build());
+        converter.setGson(gsonBuilder.create());
+        return converter;
+    }
+
+    //    private final Gson gson = new Gson();
+//
+//    // ......
+//
+//    @Override
+//    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+//        GsonHttpMessageConverter gsonHttpMessageConverter = new GsonHttpMessageConverter();
+//        gsonHttpMessageConverter.setGson(gson);
+//        converters.add(gsonHttpMessageConverter);
+//    }
+
+//    @Bean
+//    public GsonHttpMessageConverter gsonHttpMessageConverter() {
+//        GsonHttpMessageConverter converter = new GsonHttpMessageConverter();
+//        converter.setGson(gson());
+//        return converter;
+//    }
+//
+//    private Gson gson() {
+//        final GsonBuilder builder = new GsonBuilder();
+//        builder.registerTypeAdapter(Json.class, new SpringfoxJsonToGsonAdapter());
+//        return builder.create();
+//    }
+
+//    @Bean
+//    public HttpMessageConverters customConverter() {
+//        Collection<HttpMessageConverter<?>> messageConverters = new ArrayList<>();
+//        GsonHttpMessageConverter gsonHttpMessageConverter = new GsonHttpMessageConverter();
+//        messageConverters.add(gsonHttpMessageConverter);
+//        return new HttpMessageConverters(true, messageConverters);
+//    }
+
+
+    // ......
+
+
 }
