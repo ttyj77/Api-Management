871ca330a91aae291be058c3a7f1956fa92b2c45
juhee <ttyj77@naver.com>
2023-06-02 09:07:37 +0900

    06/02 데스크탑 작업물

--- src/main/java/com/ipa/openapi_inzent/config/SwaggerConfig.java
+++ src/main/java/com/ipa/openapi_inzent/config/SwaggerConfig.java
@@ -1,42 +1,59 @@
-//package com.ipa.openapi_inzent.config;
-//
-//import com.google.gson.GsonBuilder;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.http.converter.json.GsonHttpMessageConverter;
-//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-//import org.springframework.web.servlet.view.InternalResourceViewResolver;
-//import springfox.documentation.builders.ApiInfoBuilder;
-//import springfox.documentation.builders.PathSelectors;
-//import springfox.documentation.builders.RequestHandlerSelectors;
-//import springfox.documentation.service.ApiInfo;
-//import springfox.documentation.spi.DocumentationType;
-//import springfox.documentation.spring.web.json.Json;
-//import springfox.documentation.spring.web.plugins.Docket;
-//
-//@Configuration
-//@EnableWebMvc
-//public class SwaggerConfig {
-//
-//    private ApiInfo swaggerInfo() {
-//        return new ApiInfoBuilder().title("OPEN API")
-//                .description("Open Api Docs_Mydata").build();
-//    }
-//
-//    @Bean
-//    public Docket swaggerApi() {
-//        return new Docket(DocumentationType.SWAGGER_2)
-//                .apiInfo(swaggerInfo()).select()
-//                .apis(
-//                        RequestHandlerSelectors.basePackage("com.ipa.openapi_inzent")
-//                )
-//                .paths(PathSelectors.ant("/**"))
-//                .build()
-//                .useDefaultResponseMessages(false);
-//    }
-//
-//    @Bean
-//    public InternalResourceViewResolver defaultViewResolver() {
-//        return new InternalResourceViewResolver();
-//    }
-//}
+package com.ipa.openapi_inzent.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig implements WebMvcConfigurer {
+
+    private ApiInfo swaggerInfo() {
+        return new ApiInfoBuilder().title("OPEN API")
+                .description("Open Api Docs_Mydata").build();
+    }
+
+    @Bean
+    public Docket swaggerApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey()))
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.ipa.openapi_inzent"))
+                .paths(PathSelectors.ant("/mydata/**"))
+                .build()
+                .useDefaultResponseMessages(false);
+    }
+
+
+    private springfox.documentation.spi.service.contexts.SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("Authorization", authorizationScopes));
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("Authorization", "Authorization", "header");
+    }
+
+}
+
