851b6839388eab78cca2c2e7f12504eb96960a9f
(본권주희 <ttyj77@naver.com>
2023-06-07 10:26:24 +0900

    oauth2 token 적용중

--- src/main/java/com/ipa/openapi_inzent/config/SecurityConfig.java
+++ src/main/java/com/ipa/openapi_inzent/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.ipa.openapi_inzent.config;
 
 import com.ipa.openapi_inzent.config.auth.UserCustomDetailsService;
 import com.ipa.openapi_inzent.handler.UserAuthFailHandler;
-import com.ipa.openapi_inzent.handler.UserAuthSuccessHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,14 +17,14 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
-@Configuration
-@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+
+//@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 // secured Annotation 활성화 / preAuthorize Annotation 활성화
 // 일반 메소드에 @Secured("ROLE_ADMIN") 단독 설정 가능 => 신버전
 // 일반 메소드에 @PreAuthorize("hasRole('ROLE_USER') or "hasRole('ROLE_ADMIN)") 다중 설정 가능 => 구버전
 @EnableWebSecurity
+@Configuration
 public class SecurityConfig {
 
     @Bean
@@ -41,10 +40,10 @@ public class SecurityConfig {
     @Configuration
 //    @Order(2)
     public static class App1ConfigurationAdapter {
-        @Bean
-        public AuthenticationSuccessHandler userAuthSuccessHandler() {
-            return new UserAuthSuccessHandler();
-        }
+//        @Bean
+//        public AuthenticationSuccessHandler userAuthSuccessHandler() {
+//            return new UserAuthSuccessHandler();
+//        }
 
         @Bean
         public AuthenticationFailureHandler userAuthFailureHandler() {
@@ -56,29 +55,27 @@ public class SecurityConfig {
 
         @Bean
         public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
             http.csrf().disable();
             http.authorizeRequests() //authorizeRequests
-                    .antMatchers(HttpMethod.GET, "/error/*", "/login", "/login_proc", "/user/login", "/user/register", "/mydata/**", "/**").permitAll() // 설정된 url은 인증되지 않더라도 누구든 접근 가능
+                    .antMatchers(HttpMethod.GET, "/error/*", "/login", "/login_proc", "/user/login", "/user/register").permitAll() // 설정된 url은 인증되지 않더라도 누구든 접근 가능
 //                .anyRequest().authenticated()// 위 페이지 외 인증이 되어야 접근가능(ROLE에 상관없이)
-//                    .antMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-resources/**").permitAll()
+                    .antMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-resources/**").permitAll()
 //                    .antMatchers("/api/**", "/accountList", "/authorization", "/requestPage", "/user/mypage", "/mydata/**").authenticated()
-//                    .antMatchers( "/accountList", "/authorization", "/requestPage", "/user/mypage", "/mydata/**").hasRole("PROVIDER")
-//                    .antMatchers("/accountList", "/authorization", "/requestPage", "/user/mypage").hasRole("NORMAL")
-//                    .antMatchers("/mydata/**").hasRole("ADMIN")
+                    .antMatchers("/api/**", "/accountList", "/authorization", "/requestPage", "/user/mypage", "/mydata/**", "/app/main").hasRole("PROVIDER")
+                    .antMatchers("/accountList", "/authorization", "/requestPage", "/user/mypage").hasRole("USER")
+                    .antMatchers("/mydata/**").hasRole("ADMIN")
                     .and()
                     .formLogin().loginPage("/user/login")  // 접근이 차단된 페이지 클릭시 이동할 url
                     .loginProcessingUrl("/login-proc") // 로그인시 맵핑되는 url
                     .usernameParameter("username")      // view form 태그 내에 로그인 할 id 에 맵핑되는 name ( form 의 name )
                     .passwordParameter("password")      // view form 태그 내에 로그인 할 password 에 맵핑되는 name ( form 의 name )
-//                    .failureUrl("/user/login?error=true&exception=*")
-                    .successHandler(userAuthSuccessHandler()) // 로그인 성공시 실행되는 메소드
+//                    .successHandler(userAuthSuccessHandler()) // 로그인 성공시 실행되는 메소드
                     .failureHandler(userAuthFailureHandler()) // 로그인 실패시 실행되는 메소드
                     .and()
                     .logout()
                     .logoutUrl("/logout")
                     .invalidateHttpSession(true)
-                    .logoutSuccessUrl("/")
+                    .logoutSuccessUrl("/user/login")
 //                    .deleteCookies("JSESSIONID").permitAll()
                     .invalidateHttpSession(true); // 세션 clear
 
