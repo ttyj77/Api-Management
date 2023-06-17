2b5e44aacde3c95594858598ca6785a73f0ceb36
(본권주희 <ttyj77@naver.com>
2023-06-18 00:20:43 +0900

    23/06/18

---
+++ src/main/java/com/ipa/openapi_inzent/config/WebSocketConfig.java
@@ -0,0 +1,22 @@
+package com.ipa.openapi_inzent.config;
+
+import com.ipa.openapi_inzent.handler.MyWebSocketHandler;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/websocket").withSockJS();
+    }
+
+}
